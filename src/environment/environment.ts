92e130b487cf35d9fa4e176ee013469e042bb338
jonathanibarraITT <36274433+jonathanibarraITT@users.noreply.github.com>
2018-10-25 22:32:21 -0600

    first commit

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyCKIm28uzZxpGW8A8rVy421tX7oX0Ki3Cw",
+      authDomain: "dam20182-ibarra-crud.firebaseapp.com",
+      databaseURL: "https://dam20182-ibarra-crud.firebaseio.com",
+      projectId: "dam20182-ibarra-crud",
+      storageBucket: "dam20182-ibarra-crud.appspot.com",
+      messagingSenderId: "908231187591"
+  }
+};
